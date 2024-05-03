c751332fcd751b1ba191b835b3165624ac1c5256
ANDY Hormazabal <andy@andy.cl>
2024-05-03 12:26:28 -0400

    recuperacion de archivo dañado

--- src/index.php
+++ src/index.php
@@ -1,6 +1,9 @@
 <?php
-
-echo "Cambiando codigo con David Faundez";
+echo "Hola Mundo 222333444555";
+echo "Despliegue automatico ahora yaaaa!!!";
+echo "<br/>Otra prueba0211!!!";
+echo "<br/>Otra 13042024!!!";
+echo "<br/>Otra Me lo pitie en docker veamos si levanta!!!";
 
 phpinfo();
 ?>
