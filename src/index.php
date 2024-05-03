86812f51ffee1280a43ebb485c9b6a12e76e96c1
ANDY Hormazabal <andy@andy.cl>
2024-05-03 12:11:09 -0400

    haciendo cambios con david acerca de estructura de login

--- src/index.php
+++ src/index.php
@@ -1,9 +1,6 @@
 <?php
-echo "Hola Mundo 222333444555";
-echo "Despliegue automatico ahora yaaaa!!!";
-echo "<br/>Otra prueba0211!!!";
-echo "<br/>Otra 13042024!!!";
-echo "<br/>Otra Me lo pitie en docker veamos si levanta!!!";
+
+echo "Cambiando codigo con David Faundez";
 
 phpinfo();
 ?>
